c0f7100d87b97c646055e3d139033978f1cc761b
Ap5enT <95666983+Ap5enT@users.noreply.github.com>
2023-10-24 21:59:15 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from aiogram import Bot, Dispatcher, types, executor, utils
 import wikipedia
+import dotenv, os
+dotenv.load_dotenv()
 
-bot = Bot(token="5540520397:AAFEI9nOttNyJKBVZ7whFIm5qcTekIc0UnY")
+Token= os.getenv("Token")
+bot = Bot(Token)
 dp = Dispatcher(bot)
 wikipedia.set_lang('ru')
-
+Not_to_find = ["Что такое ", "что такое ", " это ","Кто такой","кто такой"]
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
@@ -14,17 +17,29 @@ async def process_start_command(message: types.Message):
 async def process_message(message: types.Message):
     try:
         chat_id = message.chat.id
-        wikitext = wikipedia.summary(message.text)
+        to_find = message.text
+        index=-1
+        for words in Not_to_find:
+            if to_find.find(words)!=None:
+                break
+            index+=1
+        if index!=-1:
+            to_find = to_find.replace(Not_to_find[index],"")
+        wikitext = wikipedia.summary(to_find)
         if len(wikitext)>500:
             wikitext = wikitext[0:1000]
             wikitext = wikitext[0:int(wikitext.rindex("."))]
         await bot.send_message(text=f'Вот что я нашел: {wikitext}',chat_id=chat_id)
+        print(f"Поиск по запросу '{to_find}' выполнен успешно ")
     except wikipedia.exceptions.DisambiguationError:
         await bot.send_message(text=f"Я нашел слишком много вариантов по запросу  '{message.text}', попробуйте сформировать запрос точнее",chat_id=chat_id)
+        print(f"Поиск по запросу '{to_find}' выполнен с ошибкой 1 ")
     except utils.exceptions.MessageIsTooLong:
             await message.reply(f"Статья слишком большая")
+            print(f"Поиск по запросу '{to_find}' выполнен с ошибкой 2 ")
     except Exception:
-        await message.reply("Извините, произошла какая-то ошибка и я не смог найти такой информации")
+        await message.reply("Извините, произошла какая-то ошибка и я не смог найти информацию по вашему запросу")
+        print(f"Поиск по запросу '{to_find}' выполнен с ошибкой -1 ")
 
 
 if __name__ == '__main__':
